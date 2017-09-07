1479936dcdde5172cfc18011f9bb65cc44150fe2
Artem Parfenenko <parfenens@gmail.com>
2017-09-08 01:52:11 +0300

    Carousel prototype added

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*$(function() {
+    console.log('suka blad 1');
+    $(document).ready(function(){
+        $(".workout-car").owlCarousel({
+            responsive: {
+                0: {
+                    items: 2
+                },
+                1360: {
+                    items: 3
+                },
+                1700: {
+                    items: 4
+                }
+            }
+        });
+    });
+    console.log('suka blad 2');
+})*/
+
+$(".owl-carousel").owlCarousel({
+    responsive: {
+        0: {
+            items: 2
+        },
+        1360: {
+            items: 3
+        },
+        1600: {
+            items: 4
+        }
+    }
+});
